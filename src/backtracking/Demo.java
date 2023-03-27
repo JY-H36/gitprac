8c7828dd0c09a426486833e0fea1c865a0f1a17c
Jy <JiaYongHu306@163.com>
2023-03-27 23:18:49 +0800

    backtracking ex

---
+++ src/backtracking/Demo.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package backtracking;
+
+/**   
+*    
+* 项目名称：Algorithm   
+* 类名称：Demo   
+* 类描述：   
+* 创建人：万重山   
+* 创建时间：2023年3月27日 下午11:16:19   
+* @version        
+*/
+public class Demo {
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
+}
